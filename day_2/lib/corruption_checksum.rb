4fe23a3afe755f4f5a083120c2d3e36be302d0b5
Ana Carlos <adscarlos@gmail.com>
2017-12-04 19:07:07 +0000

    Refactors day 2

--- day_2/lib/corruption_checksum.rb
+++ day_2/lib/corruption_checksum.rb
@@ -10,10 +10,18 @@ def get_all_differences(rows)
   rows.map { |row| difference(row)}
 end
 
+def get_input(file)
+  input_file = File.readlines(file)
 
+  input_file.map do |line|
+    line.split { |row| row.split(/\t/) }
+  end
+end
 
-input_file = File.readlines("input.txt")
+def prepare_input(input_file)
+  get_input(input_file).map { |a| a.map { |n| n.to_i } }
+end
 
-prepped_input = input_file.map { |line| line.split {|row| row.split(/\t/)}}.map { |a| a.map {|n| n.to_i}}
+input = prepare_input("input.txt")
 
-p corruption_checksum(prepped_input)
+p corruption_checksum(input)
