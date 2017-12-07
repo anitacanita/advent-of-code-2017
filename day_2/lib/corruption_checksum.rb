9efe209bdfd021baf558a6e90a0eaa6872b51eef
Ana Carlos <adscarlos@gmail.com>
2017-12-07 13:51:18 +0000

    Adds working solution for day 2 - part 2

--- day_2/lib/corruption_checksum.rb
+++ day_2/lib/corruption_checksum.rb
@@ -1,14 +1,4 @@
-def corruption_checksum(rows)
-  get_all_differences(rows).reduce(:+)
-end
-
-def difference(row)
-  row.max - row.min
-end
-
-def get_all_differences(rows)
-  rows.map { |row| difference(row)}
-end
+## work the input file ##
 
 def get_input(file)
   input_file = File.readlines(file)
@@ -24,4 +14,49 @@ end
 
 input = prepare_input("input.txt")
 
-p corruption_checksum(input)
+## part 1 ##
+
+def difference(row)
+  row.max - row.min
+end
+
+def get_all_differences(rows)
+  rows.map { |row| difference(row)}
+end
+
+def corruption_checksum(rows)
+  get_all_differences(rows).reduce(:+)
+end
+
+# p "part 1: #{corruption_checksum(input)}"
+
+## part 2
+
+def whole?(number)
+  number % 1 == 0
+end
+
+def get_evenly_divisible(row)
+  result = []
+
+  row.map do |n|
+    row.map do |n1|
+      if whole?(n/n1.to_f) && (n/n1.to_f) != 1.0
+        result << n
+        result << n1
+      end
+    end
+  end
+
+  result
+end
+
+def get_divisions(rows)
+  rows.map {|row| get_evenly_divisible(row).reduce(:/)}
+end
+
+def second_corruption_checksum(rows)
+  get_divisions(rows).reduce(:+)
+end
+
+p "part 2: #{second_corruption_checksum(input)}"
--- day_2/spec/corruption_checksum_spec.rb
+++ day_2/spec/corruption_checksum_spec.rb
@@ -1,6 +1,6 @@
 require 'corruption_checksum.rb'
 
-describe "Corruption Checksum" do
+describe "Corruption Checksum - part 1" do
   it "finds the difference between the smallest and largest number in a row" do
     expect(difference([5,1,9,5])).to eq 8
   end
@@ -12,4 +12,28 @@ describe "Corruption Checksum" do
   it "sums all the differences" do
     expect(corruption_checksum([[5,1,9,5], [7,5,3], [2,4,6,8]])).to eq 18
   end
+end
+
+describe "Corruption Checksum - part 2" do
+
+  it "knows if a number is whole" do
+    expect(whole?(6)).to be true
+  end
+
+  it "knows if a number isn't whole" do
+    expect(whole?(5.5)).to be false
+  end
+
+  it "finds the numbers that evenly divide" do
+    expect(get_evenly_divisible([5,9,2,8])).to eq [8,2]
+  end
+
+  it "saves all divisions pf multiple rows in an array" do
+    expect(get_divisions([[5,9,2,8], [9, 4, 7, 3], [3, 8, 6, 5]])).to eq [4,3,2]
+  end
+
+  it "sums all the divisions" do
+    expect(second_corruption_checksum([[5,9,2,8], [9, 4, 7, 3], [3, 8, 6, 5]])).to eq 9
+  end
+
 end
