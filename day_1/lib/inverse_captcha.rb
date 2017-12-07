26a289770d5327ef2e4baf5ce2a8f3cd78f34e5f
Ana Carlos <adscarlos@gmail.com>
2017-12-07 11:52:24 +0000

    Adds solution day 1 - part 2

--- day_1/lib/inverse_captcha.rb
+++ day_1/lib/inverse_captcha.rb
@@ -1,6 +1,7 @@
+## part 1 ##
+
 def captcha(input)
   digits_array = input.digits
-
   result = []
 
   if digits_array.first == digits_array.last
@@ -15,4 +16,26 @@ def captcha(input)
   result.inject(:+)
 end
 
-p captcha(File.read("input.txt").to_i)
+# p "part 1: #{captcha(File.read("input.txt").to_i)}"
+
+## part 2 ##
+
+def get_steps(number)
+  number.digits.count/2
+end
+
+def second_captcha(input)
+  steps = get_steps(input)
+  digits_array = input.digits
+  result = []
+
+  digits_array.map.with_index do |digit, i|
+    next_digit = digits_array[i + steps]
+    digit == next_digit ? result << digit + next_digit : result << 0
+  end
+
+  result.inject(:+)
+end
+
+
+# p "part 2: #{captcha(File.read("input.txt").to_i)}"
--- day_1/spec/inverse_captcha_spec.rb
+++ day_1/spec/inverse_captcha_spec.rb
@@ -1,6 +1,6 @@
 require "inverse_captcha.rb"
 
-describe "Inverse Captcha" do
+describe "Inverse Captcha - part 1" do
 
   it 'sums 2 matching digits' do
     expect(captcha(1111)).to eq 4
@@ -16,6 +16,22 @@ describe "Inverse Captcha" do
 
   it 'sums first and last matching digits' do
     expect(captcha(91212129)).to eq 9
+  end
+
+end
+
+describe "Inverse Captcha - part 2" do
+  it "finds the number of steps to use for digit comparison" do
+    expect(get_steps(1212)).to eq 2
+    expect(get_steps(123412)).to eq 3
+  end
+
+  it 'sums 2 matching digits' do
+    expect(second_captcha(1212)).to eq 6
+  end
+
+  it 'produces 0 when no digit matches the next' do
+    expect(second_captcha(1221)).to eq 0
   end
 
 end
